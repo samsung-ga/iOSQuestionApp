9f011763c482c81df84aa0de3bd4704c52eddb29
wody-d <maxwodyd98@gmail.com>
2022-04-29 15:35:23 +0900

    refactor: color 이름 수정

--- iOSQuestionApp/iOSQuestionApp/Global/Pallete.swift
+++ iOSQuestionApp/iOSQuestionApp/Global/Pallete.swift
@@ -7,25 +7,25 @@
 
 import UIKit
 
-struct Pallete {
-    static let main     = UIColor(named: "mainColor")
-    static let point    = UIColor(named: "pointColor")
-    static let red      = UIColor(named: "redColor")
-    static let blue     = UIColor(named: "blueColor")
-    static let gray     = UIColor(named: "grayColor")
+enum Pallete: String {
+    case main
+    case point
+    case red
+    case blue
+    case gray
+    case white
 }
 
-//enum Pallete: String {
-//    case main   = "mainColor"
-//    case point  = "pointColor"
-//    case red    = "redColor"
-//    case blue   = "blueColor"
-//    case gray   = "grayColor"
-//    case white  = "white"
-//}
-//
-//extension Pallete {
-//    var color: UIColor? {
-//        return .init(named: self.rawValue)
-//    }
+extension Pallete {
+    var color: UIColor? {
+        return .init(named: self.rawValue)
+    }
+}
+
+//struct Pallete {
+//    static let main     = UIColor(named: "mainColor")
+//    static let point    = UIColor(named: "pointColor")
+//    static let red      = UIColor(named: "redColor")
+//    static let blue     = UIColor(named: "blueColor")
+//    static let gray     = UIColor(named: "grayColor")
 //}
