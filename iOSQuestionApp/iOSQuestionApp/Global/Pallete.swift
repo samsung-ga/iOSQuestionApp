a05157a51fbba120ce15fe60bcbc2a001911efe0
wody-d <maxwodyd98@gmail.com>
2022-04-29 15:28:38 +0900

    feat: color 코드로 관리

---
+++ iOSQuestionApp/iOSQuestionApp/Global/Pallete.swift
@@ -0,0 +1,31 @@
+//
+//  Color.swift
+//  iOSQuestionApp
+//
+//  Created by Woody on 2022/04/29.
+//
+
+import UIKit
+
+struct Pallete {
+    static let main     = UIColor(named: "mainColor")
+    static let point    = UIColor(named: "pointColor")
+    static let red      = UIColor(named: "redColor")
+    static let blue     = UIColor(named: "blueColor")
+    static let gray     = UIColor(named: "grayColor")
+}
+
+//enum Pallete: String {
+//    case main   = "mainColor"
+//    case point  = "pointColor"
+//    case red    = "redColor"
+//    case blue   = "blueColor"
+//    case gray   = "grayColor"
+//    case white  = "white"
+//}
+//
+//extension Pallete {
+//    var color: UIColor? {
+//        return .init(named: self.rawValue)
+//    }
+//}
