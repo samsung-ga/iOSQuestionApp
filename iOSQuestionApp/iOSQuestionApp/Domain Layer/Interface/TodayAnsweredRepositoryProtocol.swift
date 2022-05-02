9aea5548c92e5580edcdf12699237108b15cecd4
wody-d <maxwodyd98@gmail.com>
2022-05-03 08:50:55 +0900

    feat: Repository 추가

---
+++ iOSQuestionApp/iOSQuestionApp/Domain Layer/Interface/TodayAnsweredRepositoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  TodayAnsweredRepositoryProtocol.swift
+//  iOSQuestionApp
+//
+//  Created by Woody on 2022/05/02.
+//
+
+import Foundation
+
+// TODO: 도메인 내 모델 Requeset Response 형태로 수정 필요
+protocol TodayAnsweredRepositoryProtocol {
+    func isAnsweredToday() -> Bool
+}
