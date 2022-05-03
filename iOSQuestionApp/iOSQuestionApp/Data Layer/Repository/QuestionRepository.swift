b06f17796a31ce02fc15fdc5393ebb11eaecabcd
wody-d <maxwodyd98@gmail.com>
2022-05-03 16:00:30 +0900

    refactor: 주석 수정

--- iOSQuestionApp/iOSQuestionApp/Data Layer/Repository/QuestionRepository.swift
+++ iOSQuestionApp/iOSQuestionApp/Data Layer/Repository/QuestionRepository.swift
@@ -17,9 +17,9 @@ final class QuestionRepository {
     
 }
 
+// TODO: 쿼리로직이 Repository에 들어가게 되는 문제점
 extension QuestionRepository: QuestionRepositoryProtocol {
     
-    // TODO: 비즈니스 로직으로 처리할까? 나중에 고민해보자... 근데 진짜 로직이 간단해서 구분하기가 오히려 더 어렵다 ㅠ ㅠ
     func getRandomQuestion() -> Question? {
         if let questions = questionTable.getRow() {
             return questions.filter { $0.isAnswered == false }.randomElement()
@@ -27,7 +27,6 @@ extension QuestionRepository: QuestionRepositoryProtocol {
         return nil
     }
     
-    // TODO: 비즈니스 로직에서 최근에 답한 질문들부터 위에 오도록 정렬해주어야한다. 그러기위해선 답변들도 함께 뽑아야함.
     func getQuestionsAnswered() -> [Question] {
         if let questions = questionTable.getRow() {
             return questions.filter { $0.isAnswered == true }
@@ -35,5 +34,4 @@ extension QuestionRepository: QuestionRepositoryProtocol {
         return []
     }
     
-    // TODO: 질문을 어떤 식으로 집어넣을 지도 고민해보아야 함. JSON을 통째로 교체하면 절대로 안됌(답변여부를 체크해서 넣어야함)
 }
